73e977494932c8ea947cf542195962ba9a985bc4
Juan Alberto López Onofre <JuanAlbertoLopezOnofre@outlook.com>
2023-10-27 09:54:31 -0600

    Add project files.

---
+++ TestXamarin/Test/Init/AppInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.UITest;
+using Xamarin.UITest.Utils;
+
+namespace TestXamarin.Test.Init
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+                return ConfigureApp
+                         .Android
+                         .ApkFile("D:/Selenium in c#/UnitTestXamarin/UnitTestXamarin/UnitTestXamarin.Android/bin/Release/com.companyname.unittestxamarin.apk")
+                         .WaitTimes(new WaitTimes())
+                         .StartApp();
+            return ConfigureApp.iOS
+                                .WaitTimes(new WaitTimes())
+                                .StartApp();
+        }
+
+        public class WaitTimes : IWaitTimes
+        {
+            public TimeSpan GestureCompletionTimeout => TimeSpan.FromMinutes(1);
+
+            public TimeSpan GestureWaitTimeout => TimeSpan.FromMinutes(1);
+
+            public TimeSpan WaitForTimeout => TimeSpan.FromMinutes(1);
+        }
+    }
+}
---
+++ TestXamarin/Test/LoginTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestXamarin.Test.Init;
+using Xamarin.UITest;
+
+namespace TestXamarin.Test
+{
+    [TestFixture(Platform.Android)]
+    public class LoginTest
+    {
+        IApp app;
+        Platform platform;
+
+        public LoginTest(Platform platform) => this.platform = platform;
+
+        [SetUp]
+        public void BeforeEachTest()=> app = AppInitializer.StartApp(platform);
+
+        [Test]
+        public void ShouldBeAbleToLogin()
+        {
+            Login();
+        }
+        public void Login()
+        {
+            app.Tap("entryNameId");
+            app.EnterText("Juan");
+            app.DismissKeyboard();
+        }
+    }
+}
